4b99f8c6b0108936a17c8a79058411772e7adec9
Mikhail Nazarenko <mikhail.nazarenko@gmail.com>
2016-02-17 16:21:41 +0300

    Change L2-watchdog.sh Commit

--- radiofid-openwrt-14.07-899484766f7b/files/root/scripts/L2-watchdog.sh
+++ radiofid-openwrt-14.07-899484766f7b/files/root/scripts/L2-watchdog.sh
@@ -4,6 +4,10 @@ DEV=L2
 POINT=`uci get network.@ospf[0].id`
 DEST=`uci get network.@tun[2].server|awk -F "/" '{print $1}'`
 
+[ -f /tmp/wdog.txt ] || {
+  echo '3' > /tmp/wdog.txt
+}
+
 if ip link show $DEV | grep -q "UP"  
    then
 	tries=0
@@ -13,9 +17,19 @@ if ip link show $DEV | grep -q "UP"
 		then
 			exit 0
 		fi
-		tries=$((tries+1))
+        	tries=$((tries+1))
 	done
+	wdog=`expr $wdog - 1`;
+	echo $wdog > /tmp/wdog.txt
 fi
+
+wdog=`cat /tmp/wdog.txt`
+[ $wdog -le 0 ] && {
+  logger "Reboot planned"
+  [ `cat /proc/uptime | awk -F . '{ print $1 }'` -gt 3600 ] && {
+    reboot
+  }
+}
 
 logger "Restart L2 tunnel from: $POINT to: $DEST"
 /root/scripts/L2-down.sh
